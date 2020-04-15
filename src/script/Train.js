a53b4055692ce2716bfe12e3662e08e5d3ebae3e
SkaymanT <SkaymanT@yandex.ru>
2020-04-15 17:56:04 +0300

    feat: add 2 page in train

---
+++ src/script/Train.js
@@ -0,0 +1,36 @@
+import CARDS from './cards.js';
+
+export default class Train {
+  constructor(train) {
+    this.train = train;
+  }
+
+  createTrain(category) {
+    let keyValue = '<div class="rating none"></div>';
+    this.train.insertAdjacentHTML('beforeend', keyValue);
+    CARDS[category].forEach((key, index) => {
+      console.log(category);
+      let card = CARDS[1][index];
+      let link = './assets/';
+      let word = '';
+      let translation = '';
+
+      for (let keyCard in card) {
+        if (keyCard == 'image') {
+          link += card[keyCard];
+        }
+        if (keyCard == 'word') {
+          word = card[keyCard];
+        }
+        if (keyCard == 'translation') {
+          translation = card[keyCard];
+        }
+      }
+      keyValue = `<div class="card-container"><div class="card"><div class="front" style="background-image: url(${link});"><div class="card-header">${word}</div></div><div class="back" style="background-image: url(${link}"><div class="card-header">${translation}</div></div><div class="rotate"></div></div></div>`;
+      this.train.insertAdjacentHTML('beforeend', keyValue);
+    });
+  }
+
+
+
+}
--- src/script/home.js
+++ src/script/home.js
@@ -1,15 +1,21 @@
 import '../style/style.scss';
 import Header from './header.js';
 import Main from './main.js';
-import Train from './main.js';
-import Play from './main.js';
+import Train from './train.js';
+import Play from './play.js';
 import '../script/cards.js';
+
+
+window.onload = () => {
+  const page = new Page();
+  page.createPage();
+  page.addListenersOnKeys();
+};
 
 class Page {
   constructor() {
-    this.isStatus = true;
+    this.isMode = true;
   }
-
   createPage() {
     const body = document.querySelector('body');
     this.root = document.createElement('div');
@@ -35,38 +41,76 @@ class Page {
     body.prepend(this.root);
   }
 
-
-  handlerSwitch() {
-    console.log( this.isStatus);
-    if (this.isStatus) {
-      document.getElementsByClassName('menu').forEach(element => element.classList.remove('green'));
-      document.getElementsByClassName('main-card').forEach(element => element.classList.remove('green'));
-    } else {
-      document.getElementsByClassName('menu').forEach(element => element.classList.add('green'));
-      document.getElementsByClassName('main-card').forEach(element => element.classList.add('green'));
-    }
-    this.isStatus = !this.isStatus;
-  }
-
-  handlerMouseDown(event) {
-
-  }
-
-  handlerMouseUp() {
-
-  }
-
-
   addListenersOnKeys() {
-    document.getElementsByClassName('switch')[0].addEventListener("mousedown", (event) => this.handlerSwitch());
-    document.addEventListener("mousedown", (event) => this.handlerMouseDown(event));
-    document.addEventListener("mouseup", () => this.handlerMouseUp());
+    document.addEventListener('click', (event) => this.handlerClick(event));
+  }
+
+  handlerClick(event) {
+    if (this.isBlurMenu(event)) {
+      this.blurMenu();
+    }
+
+    if (this.isSwitchMode(event)) {
+      this.switchMode();
+    }
+
+    if (this.isClickOnCard(event)) {
+      this.clickOnCard();
+    }
+  }
+
+  isBlurMenu(event) {
+    if (!event.target.classList.contains('menu') && event.target.tagName !== 'INPUT') {
+      return true;
+    }
+  }
+
+  blurMenu() {
+    document.querySelector('.menuToggle>input').checked = false;
+  }
+
+  isSwitchMode(event) {
+    if (event.target.parentNode.className == 'switch' && event.target.tagName =='SPAN') {
+      return true;
+    }
+  }
+
+  switchMode() {
+    let menu = document.querySelectorAll('.menu');
+    let cards = document.querySelectorAll('.main-card');
+    if (this.isMode) {
+      document.querySelector('.switch-input').setAttribute('checked', '');
+      for (var i = 0; i < menu.length; i++) {
+        menu[i].classList.remove('green');
+      }
+      for (var i = 0; i < cards.length; i++) {
+        cards[i].classList.remove('green');
+      }
+    } else {
+      document.querySelector('.switch-input').removeAttribute('checked');
+      for (var i = 0; i < menu.length; i++) {
+        menu[i].classList.add('green');
+      }
+      for (var i = 0; i < cards.length; i++) {
+        cards[i].classList.add('green');
+      }
+    }
+    this.isMode = !this.isMode;
+  }
+
+  isClickOnCard(event) {
+    if (event.target.parentNode.classList.contains('main-card') || event.target.classList.contains('main-card')) {
+      return true;
+    }
+  }
+
+  clickOnCard() {
+    this.container.remove();
+    this.container = document.createElement('div');
+    this.container.classList.add('container');
+    this.appcontainer.append(this.container);
+    const train = new Train(this.container);
+    train.createTrain(3); 
   }
 
 }
-
-window.onload = () => {
-  const page = new Page();
-  page.createPage();
-  page.addListenersOnKeys();
-};
