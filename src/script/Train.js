4d2e8130085f74cad52a653c55130cf3071874d5
SkaymanT <SkaymanT@yandex.ru>
2020-04-15 22:54:58 +0300

    feat: update dist for deploy

--- src/script/Train.js
+++ src/script/Train.js
@@ -29,6 +29,15 @@ export default class Train {
       keyValue = `<div class="card-container"><div class="card"><div class="front" style="background-image: url(${link});"><div class="card-header">${word}</div></div><div class="back" style="background-image: url(${link}"><div class="card-header">${translation}</div></div><div class="rotate"></div></div></div>`;
       this.train.insertAdjacentHTML('beforeend', keyValue);
     });
+    keyValue = '<div class="btns"><button class="btn none">Start game</button></div>';
+    this.train.insertAdjacentHTML('beforeend', keyValue);
+    keyValue = '<audio class="audio" src="/assets/audio/cry.mp3"></audio>';
+    this.train.insertAdjacentHTML('beforeend', keyValue);
+    keyValue = '<audio class="soundEffects"></audio>';
+    this.train.insertAdjacentHTML('beforeend', keyValue);
+    let audioElement = new Audio('angry.mp3');
+    audioElement.play();
+
   }
 
 
