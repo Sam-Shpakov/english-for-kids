292b98486cf6c6ab88a2ef9af2b977dbd542349b
SkaymanT <SkaymanT@yandex.ru>
2020-04-25 22:11:57 +0300

    fix: fix bugs

--- src/script/home.js
+++ src/script/home.js
@@ -547,16 +547,22 @@ class Page {
       for (let i = 0; i < cards.length; i++) {
         cards[i].style.display = 'none';
       }
+      let audio = document.querySelector('.audio');
+      audio.setAttribute('src', '../assets/audio/success.mp3');
+      audio.play();
       document.querySelector('body').classList.add('succes');
       document.querySelector('.btns').style.display = 'none';
       document.querySelector('.switch-container').style.display = 'none';
       setTimeout(() => {
         this.returnToMain();
-      }, 3000);
+      }, 100000);
     } else {
       document.querySelector('.rating').remove();
       let keyValue = `<div class="rating" style="justify-content: center;">${this.numberErrors} Errors</div>`;
       this.container = document.querySelector('.container');
+      let audio = document.querySelector('.audio');
+      audio.setAttribute('src', '../assets/audio/failure.mp3');
+      audio.play();
       this.container.insertAdjacentHTML('beforebegin', keyValue);
       let cards = document.querySelectorAll('.card');
       for (let i = 0; i < cards.length; i++) {
@@ -567,7 +573,7 @@ class Page {
       document.querySelector('.switch-container').style.display = 'none';
       setTimeout(() => {
         this.returnToMain();
-      }, 3000);
+      }, 100000);
     }
   }
 
